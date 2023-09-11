ced9843ba850204d3b3070aca3fe7a52d9a6b479
zhouzhou <zhouzzhou0619@gmail.com>
2023-09-11 21:17:21 +1000

    import classnames

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,7 @@
-import PropTypes from 'prop-types';
+import className from 'classnames';
+
 function Button({ children, primary, secondary, success, warning, danger, outline, rounded }) {
-    return <button>{children}</button>;
+    return <button className="px-3 py-1.5 border border-blue-600 bg-blue-500 text-white">{children}</button>;
 }
 Button.propTypes = {
     checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
