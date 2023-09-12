4d5df634b573586bf3119f8164f9304e9a919870
zhouzhou <zhouzzhou0619@gmail.com>
2023-09-12 18:21:17 +1000

    create pages and components folders

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -1,6 +1,6 @@
-import Button from './components/Button';
+import Button from '../components/Button';
 import { GoBell, GoCloud, GoFileDirectoryFill } from 'react-icons/go';
-function App() {
+function ButtonPage() {
     return (
         <div>
             <div>
@@ -35,4 +35,4 @@ function App() {
     );
 }
 
-export default App;
+export default ButtonPage;
